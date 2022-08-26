df2501efc8ca9e63d84e8d6a52e5156a60db43c0
Gabriel Santo <gvgesanto2@gmail.com>
2022-08-26 09:52:35 -0300

    feat: Create the max function

--- my-enumerable.rb
+++ my-enumerable.rb
@@ -26,4 +26,39 @@ module MyEnumerable
       self
     end
   end
+
+  def max(n=nil, &block)
+    n ? get_max_values(n, &block) : get_max_value(list, &block)      
+  end
+
+  private
+
+  def get_max(list, &max_test)
+    max = list[0]
+
+    list.each do |element|  
+      max = element if max_test.call(element, max)
+    end
+    max
+  end
+
+  def get_max_value(list)
+    max_test = block_given? ? 
+      lambda { |x, y| yield(x, y) == 1 } : 
+      lambda { |x, y| x > y }
+
+    get_max(list, &max_test)
+  end
+
+  def get_max_values(n, &block)
+    tmp_list = list
+    result = []
+
+    n.times do
+      max = get_max_value(tmp_list, &block)
+      tmp_list.delete_at(tmp_list.find_index(max))
+      result << max
+    end
+    result
+  end
 end
--- my-list.rb
+++ my-list.rb
@@ -13,7 +13,7 @@ class MyList
   end
 end
 
-a = MyList.new(2, -5, 6, 8, 3, 8)
+a = MyList.new(-2, 5, 6, 8, -1, 8, 8, 3)
 
 p a.list
 
@@ -23,9 +23,18 @@ puts a.all? {|e| e < 5}
 puts a.all? {|e| nil }
 
 puts "Testing .any?"
-puts a.any? {|e| e == 2}
+puts a.any? {|e| e == -2}
 puts a.any? {|e| e == 999}
 
 puts "Testing filter"
 p a.filter {|e| e.even?}
 p a.filter {|e| e.odd?}
+p a.filter {|e| e < 0 }
+
+puts "Testing max"
+puts a.max
+p a.max(5)
+
+b = MyList.new('albatross', 'dog', 'horse', 'eagle', 'buffalo')
+puts b.max {|a, b| a.length <=> b.length}
+p b.max(2) {|a, b| a.length <=> b.length}
